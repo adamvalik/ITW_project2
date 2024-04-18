21a8533200f7d0da9aa51992c4aba05cdd9cda51
adamvalik <xvalik05@stud.fit.vutbr.cz>
2024-04-18 18:16:52 +0200

    home page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// script.js
+// active section in the sticky menu
 window.addEventListener('scroll', function() {
     const sections = document.querySelectorAll('section');
     const navLinks = document.querySelectorAll('nav ul li a');
@@ -8,15 +8,53 @@ window.addEventListener('scroll', function() {
     sections.forEach(section => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
-        if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+        if (window.scrollY >= (sectionTop - sectionHeight / 3)) {
             current = section.getAttribute('id');
         }
     });
 
     navLinks.forEach(link => {
         link.classList.remove('active');
-        if (link.href.includes(current)) {
+        if (link.getAttribute('href').includes('#' + current)) {
             link.classList.add('active');
         }
     });
+});
+
+
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.scrollY - 55; // height of the sticky menu
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const roles = ['student', 'programmer', 'developer', 'runner', 'orienteerer', 'rogalo', 'batman'];
+    const animatedText = document.getElementById('animated');
+    let currentIndex = 0;
+
+    function updateText() {
+        animatedText.style.opacity = 0;
+
+        setTimeout(() => {
+            animatedText.textContent = roles[currentIndex];
+            animatedText.style.opacity = 1;
+            currentIndex = (currentIndex + 1) % roles.length;
+        }, 500); // Wait for the fade out to complete before changing the text
+
+        setTimeout(updateText, 3000); // Change text every 3 seconds
+    }
+
+    setTimeout(updateText, 1000); // Start the animation cycle
 });
