4ec459b420c4003b3125830cbac1d130fa84f664
adamvalik <xvalik05@stud.fit.vutbr.cz>
2024-04-18 12:58:47 +0200

    uf

---
+++ script.js
@@ -0,0 +1,22 @@
+// script.js
+window.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.href.includes(current)) {
+            link.classList.add('active');
+        }
+    });
+});
