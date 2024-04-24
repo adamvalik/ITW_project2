31c5c2b1db3296270bcaeeef2f7bc1575cf14718
adamvalik <xvalik05@stud.fit.vutbr.cz>
2024-04-24 16:46:49 +0200

    final?

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// active section in the sticky menu
 window.addEventListener('scroll', function() {
     const sections = document.querySelectorAll('section');
     const navLinks = document.querySelectorAll('nav ul li a');
@@ -17,14 +16,6 @@ window.addEventListener('scroll', function() {
         link.classList.remove('active');
         if (link.getAttribute('href').includes('#' + current)) {
             link.classList.add('active');
-        }
-    });
-
-    sections.forEach(section => {
-        if (section.getAttribute('id') === current) {
-            section.classList.add('active');
-        } else {
-            section.classList.remove('active');
         }
     });
 });
@@ -54,8 +45,63 @@ document.addEventListener('DOMContentLoaded', function () {
 document.addEventListener('DOMContentLoaded', (event) => {
     const burger = document.querySelector('.burger');
     const links = document.querySelector('.links');
+    const menuLinks = links.querySelectorAll('a'); // Assuming your links are <a> elements
 
+    // Toggle menu visibility when the hamburger icon is clicked
     burger.addEventListener('click', () => {
         links.classList.toggle('active');
     });
+
+    // Add an event listener to each link in the menu
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            // Close the menu when a link is clicked
+            if (links.classList.contains('active')) {
+                links.classList.remove('active');
+            }
+        });
+    });
 });
+
+
+function isElementVisible(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+function animateCounterIfVisible() {
+    document.querySelectorAll('.counter').forEach(counter => {
+        if (isElementVisible(counter) && !counter.classList.contains('animated')) {
+        animateValue(counter, parseInt(counter.dataset.target));
+        counter.classList.add('animated');
+        }
+    });
+}
+
+function animateValue(obj, end) {
+    let current = parseInt(obj.textContent);
+    const increment = end > current ? 6 : -1;
+    
+    const step = () => {
+        current += increment;
+        obj.textContent = current;
+        
+        if ((increment > 0 && current < end) || (increment < 0 && current > end)) {
+            setTimeout(step, 1);
+        }
+    };
+    
+    step();
+}
+
+// Listen for scroll events
+window.addEventListener('scroll', animateCounterIfVisible);
+
+// Initial check
+animateCounterIfVisible();
+  
